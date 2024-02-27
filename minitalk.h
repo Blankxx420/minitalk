2af90676fd7b171d9fbae843d9cbb652e17c27df
Blankxx420 <briceguichou.dev@gmail.com>
2024-02-27 09:14:46 +0100

    start server side

--- minitalk.h
+++ minitalk.h
@@ -6,12 +6,12 @@
 /*   By: brguicho <brguicho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 13:56:22 by brguicho          #+#    #+#             */
-/*   Updated: 2024/02/23 13:58:32 by brguicho         ###   ########.fr       */
+/*   Updated: 2024/02/26 17:34:10 by brguicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
+#ifndef MINITALK_H_
+# define MINITALK_H_
 
 # include <signal.h>
 # include "libft/libft.h"
--- server.c
+++ server.c
@@ -6,13 +6,31 @@
 /*   By: brguicho <brguicho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 13:37:52 by brguicho          #+#    #+#             */
-/*   Updated: 2024/02/23 14:22:01 by brguicho         ###   ########.fr       */
+/*   Updated: 2024/02/26 17:36:38 by brguicho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-int main()
+void	print_pid(void)
 {
+	ft_putstr_fd("PID : ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		ft_printf("Error : no arguments requiere\n");
+		return(1);
+	}
+	else
+	{	(void) argv;
+		print_pid();
+		while (42)
+			pause();
+	}
 	return (0);
 }
